fb6b75b8605e56a7b20b7349ecaff2ad914ddd05
Nickisthebestatcoding <nickooiman06@gmail.com>
2025-04-07 10:16:16 -0500

    Update PortalBehavior.cs

--- Wizard Arena Game/Assets/Scripts/PortalBehavior.cs
+++ Wizard Arena Game/Assets/Scripts/PortalBehavior.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PortalBehavior : MonoBehaviour
+public class Teleporter
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera mainCamera;
+
+    private float xMin;
+    private float yMin;
+    private float xMax;
+    private float yMax;
+
+    public Teleporter(Camera cam)// Start is called before the first frame update
     {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    
+    
         
-    }
+    
 }
