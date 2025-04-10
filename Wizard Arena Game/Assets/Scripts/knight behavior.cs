8050df3cf1e8c74ecd01f8485ca057e71556c64b
Cat-is-Dog <kavyansh.ravrani@gmail.com>
2025-04-10 10:02:33 -0500

    g

--- Wizard Arena Game/Assets/Scripts/knight behavior.cs
+++ Wizard Arena Game/Assets/Scripts/knight behavior.cs
@@ -43,7 +43,12 @@ public class knightbehavior : MonoBehaviour
             Vector3 movement = direction.normalized * moveSpeed * Time.deltaTime;
             transform.position += movement;
         }
-
+        // Rotate to face the wizard
+        if (direction.sqrMagnitude > 0.01f)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
 
     }
 }
