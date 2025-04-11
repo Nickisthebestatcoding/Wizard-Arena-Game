9e860367750005a2c5eba969af77ae425aa5a9db
Nickisthebestatcoding <nickooiman06@gmail.com>
2025-04-11 10:50:15 -0500

    Update PortalController.cs

--- Wizard Arena Game/Assets/Scripts/PortalController.cs
+++ Wizard Arena Game/Assets/Scripts/PortalController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PortalController : MonoBehaviour
 {
+    public GameObject ball;
+
+    float portalRadius;
+
+    bool scored;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer r = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
