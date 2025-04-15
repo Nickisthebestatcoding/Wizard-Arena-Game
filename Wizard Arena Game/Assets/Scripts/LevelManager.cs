d9dc47eba7b0a7835304b4872b7c1c983051ed81
Cat-is-Dog <kavyansh.ravrani@gmail.com>
2025-04-15 08:59:30 -0500

    Update LevelManager.cs

--- Wizard Arena Game/Assets/Scripts/LevelManager.cs
+++ Wizard Arena Game/Assets/Scripts/LevelManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager Instance;
+
+    public Transform playerSpawnPoint;
+    public GameObject player;
+    private Vector3 playerStartPos;
+    private Quaternion playerStartRot;
+    private Health playerHealth;
+
+    private List<knightbehavior> enemies = new List<Enemy>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,42 @@ public class LevelManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        if (player != null)
+        {
+            playerStartPos = playerSpawnPoint.position;
+            playerStartRot = playerSpawnPoint.rotation;
+            playerHealth = player.GetComponent<Health>();
+        }
+
+        // Store all enemy references
+        enemies.AddRange(FindObjectsOfType<Enemy>()); // Make sure your enemies use a common Enemy script
+    }
+    public void ResetLevel()
+    {
+        // Reset player
+        player.transform.position = playerStartPos;
+        player.transform.rotation = playerStartRot;
+
+        if (playerHealth != null)
+        {
+            playerHealth.ResetHealth(); // We’ll add this function next
+        }
+
+        // Reset enemies
+        foreach (var enemy in enemies)
+        {
+            enemy.ResetEnemy(); // We’ll add this too
+        }
+
+        Debug.Log("Level reset. Timer unchanged.");
     }
 }
