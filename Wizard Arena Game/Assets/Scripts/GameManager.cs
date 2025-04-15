bc3a03ec608c66d997daa127f40881f51984a8f7
Nickisthebestatcoding <nickooiman06@gmail.com>
2025-04-15 10:00:45 -0500

    1

--- Wizard Arena Game/Assets/Scripts/GameManager.cs
+++ Wizard Arena Game/Assets/Scripts/GameManager.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
+
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text coinText; // Reference to the UI text to show coin count
+    private int coinCount = 0;
+    
+    // Call this method to increase the coin count
+    public void AddCoin()
     {
-        
+        coinCount++; // Increment the coin count
+        UpdatecoinText(); // Update the UI text
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update the coin count display in the UI
+    private void UpdatecoinText()
     {
-        
+        coinText.text = "Coins: " + coinCount; // Update the UI text
     }
 }
