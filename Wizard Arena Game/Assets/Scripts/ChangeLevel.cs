f5c8d85e637142a45b573809337ff13f3f137d27
Nickisthebestatcoding <nickooiman06@gmail.com>
2025-04-15 10:53:22 -0500

    Update ChangeLevel.cs

--- Wizard Arena Game/Assets/Scripts/ChangeLevel.cs
+++ Wizard Arena Game/Assets/Scripts/ChangeLevel.cs
@@ -1,36 +1,15 @@
-using System.Collections;
-using System;
 using UnityEngine;
-using TMPro;
 using UnityEngine.SceneManagement;
 
 public class ChangeLevel : MonoBehaviour
 {
-    string[] levelNames = {"TutorialWorld", "Level1"};
-    int currentLevel = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        SceneManager.LoadScene("TutorialWorld", LoadSceneMode.Additive);
-    }
+    public string Level1;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-    public void Changelevel()
-    {
-        string oldscene = levelNames[currentLevel];
-
-        currentLevel++;
-        if (currentLevel >= levelNames.Length)
+        if (other.CompareTag("Wizard"))
         {
-            currentLevel = 0;
+            SceneManager.LoadScene(Level1);
         }
-
-        SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
-
-        SceneManager.UnloadSceneAsync("TutorialWorld");
     }
 }
