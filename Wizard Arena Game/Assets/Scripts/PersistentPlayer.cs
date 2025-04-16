2f019502ff0b9f44b8820b6f7bb6acc3bc4f0d0a
Nickisthebestatcoding <nickooiman06@gmail.com>
2025-04-16 09:39:58 -0500

    1

---
+++ Wizard Arena Game/Assets/Scripts/PersistentPlayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PersistentPlayer : MonoBehaviour
+{
+    private static PersistentPlayer instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
