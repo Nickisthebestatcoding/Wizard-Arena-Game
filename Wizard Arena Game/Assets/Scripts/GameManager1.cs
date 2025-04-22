d6d5ff21d8a2705894234a2bd2d33f74bf8727ef
Nickisthebestatcoding <nickooiman06@gmail.com>
2025-04-22 10:01:02 -0500

    Update GameManager1.cs

--- Wizard Arena Game/Assets/Scripts/GameManager1.cs
+++ Wizard Arena Game/Assets/Scripts/GameManager1.cs
@@ -1,13 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager1 : MonoBehaviour
 {
     public GameObject CompletedText;
+    public float displayTime = 3f;
 
     public void ShowGameOver()
     {
         CompletedText.SetActive(true);
+    }
+    void HandleEnemyDefeated()
+    {
+        StartCoroutine(ShowCompletedTextForSeconds(displayTime));
+    }
+
+    IEnumerator ShowCompletedTextForSeconds(float seconds)
+    {
+        CompletedText.SetActive(true);
+        yield return new WaitForSeconds(3);
+        CompletedText.SetActive(false);
     }
 
 }
