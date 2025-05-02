0c7ad9e402e25cb38ddcf8b0b736d9e62c9feaa0
Nickisthebestatcoding <nickooiman06@gmail.com>
2025-05-02 09:32:25 -0500

    Update WizardCoinManager.cs

--- Wizard Arena Game/Assets/Scripts/WizardCoinManager.cs
+++ Wizard Arena Game/Assets/Scripts/WizardCoinManager.cs
@@ -14,6 +14,7 @@ public class WizardCoinManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
